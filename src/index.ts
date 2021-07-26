f0ffb84a9a55c50bcf966ff8ca78f41e0a0dd408
Ruben Montes <rubenmontes330@gmail.com>
2021-07-26 13:23:25 +0100

    funciones basicas

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,15 @@
 /*
     ===== Código de TypeScript =====
 */
+function sumar(a:number, b:number): number{
+    return (a+b);
+}
+
+const sumarflecha = (a:number,b:number):number => {return a+b}
+
+function multiplicar (numero:number,base:number=10,otronumero?:number){
+    return numero*base;
+}
+
+const result= multiplicar(1);
+console.log(result);
