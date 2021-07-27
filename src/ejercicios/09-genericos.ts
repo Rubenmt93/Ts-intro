7864acd33cf403f3c26e171c2a1283ae609d5858
Ruben Montes <rubenmontes330@gmail.com>
2021-07-27 13:26:11 +0100

    Genericos2

--- src/ejercicios/09-genericos.ts
+++ src/ejercicios/09-genericos.ts
@@ -4,3 +4,8 @@
 /*
     ===== Código de TypeScript =====
 */
+function queTipoSoy<T>(argumento:T) {
+    return argumento;
+}
+let soyString= queTipoSoy("hola mundo");
+let soyNumerog= queTipoSoy(100);
