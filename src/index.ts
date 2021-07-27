ca55540fec795c32b2d004301246ed80a808fe04
Ruben Montes <rubenmontes330@gmail.com>
2021-07-27 13:02:20 +0100

    Constructor

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,11 @@
 /*
     ===== Código de TypeScript =====
 */
+class Heroe{
+    
+    constructor(public alterEgo:string,public edad:number, public nombreReal:string){
+        
+    }
+}
+const ironman= new Heroe("ironman",45,"Tony Stark");
+console.log(ironman);
